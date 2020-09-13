2e507b409170f4683c60c056519369b79e9190fc
rock <rockcreation7@gmail.com>
2020-09-13 22:56:11 +0800

    update http

--- roster-client/src/http-common.js
+++ roster-client/src/http-common.js
@@ -1,8 +1,8 @@
-import axios from "axios"; 
+import axios from "axios"
 
 export default axios.create({
   // Development
-  baseURL: "http://localhost:8000/api"
+  // baseURL: "http://localhost:8000/api"
   // Production
-  // baseURL: "/api"
-});
+  baseURL: "/api",
+})
